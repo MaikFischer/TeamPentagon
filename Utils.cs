ba1a2e696164bb748ebafc9b5f4969084739e95b
vapez2k <lolman906@gmail.com>
2020-11-15 18:31:26 +0100

    Added Base GUI structure and development

---
+++ Utils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditClicker
+{
+    class Utils
+    {
+        private string version = "v1.1";
+        private string title = "CreditClicker";
+
+        public string getVersion()
+        {
+            return this.version;
+        }
+        public string getTitle()
+        {
+            return this.title;
+        }
+    }
+}

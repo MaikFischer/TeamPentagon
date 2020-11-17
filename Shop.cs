8e72249d364502f1d6c078f638d5e81ba060173f
vapez2k <lolman906@gmail.com>
2020-11-17 22:37:51 +0100

    Finished Shop Logic fixes #4

--- Shop.cs
+++ Shop.cs
@@ -12,9 +12,28 @@ namespace CreditClicker
 {
     public partial class Shop : Form
     {
-        public Shop()
+
+        private Game game;
+
+        //Initialisierung aller Items
+        //CheatSheet = +1 Bonus pro Klick +0 Multiplikator Preis: 1000
+        private Item cheatSheet = new Item("CheatSheet",1,0,1000);
+
+        public Shop(Game game)
         {
+            this.game = game;
             InitializeComponent();
+        }
+
+        private void buyCheatSheet_Click(object sender, EventArgs e)
+        {
+            if (game.getScore() >= cheatSheet.getPrice()) 
+            {
+                game.setScore(game.getScore() - cheatSheet.getPrice());
+                game.addItem(cheatSheet);
+                game.raiseMultiplier(cheatSheet);
+                game.updateScore();
+            }
         }
     }
 }
